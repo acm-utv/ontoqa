22cfdacd8fb1e9090873f6debf98bfba1d922b14
Giacomo Marciani <giacomo.marciani@gmail.com>
2016-11-30 13:07:38 +0100

    lesson examples #1

---
+++ src/main/java/com/acmutv/ontoqa/examples/Example1C.java
@@ -0,0 +1,48 @@
+/**
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2016 Antonella Botte, Giacomo Marciani and Debora Partigianoni
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.acmutv.ontoqa.examples;
+
+import org.eclipse.rdf4j.model.Model;
+import org.eclipse.rdf4j.rio.RDFFormat;
+import org.eclipse.rdf4j.rio.Rio;
+
+import java.io.IOException;
+
+/**
+ * This class realizes ...
+ * @author Giacomo Marciani {@literal <gmarciani@acm.org>}
+ * @since 1.0.0
+ * @see
+ */
+public class Example1C {
+
+  public static void main(String[] args) throws IOException {
+    Model model =
+        Rio.parse(Example1C.class.getResourceAsStream("/examples/example1C.ttl"), "http://example.org", RDFFormat.TURTLE);
+
+    Rio.write(model, System.out, RDFFormat.RDFXML);
+  }
+}
