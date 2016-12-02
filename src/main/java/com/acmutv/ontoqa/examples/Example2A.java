5fd3c96cb75a8b789ec6df010fbd487b6696f821
Giacomo Marciani <giacomo.marciani@gmail.com>
2016-12-02 16:28:53 +0100

    Examples lesson #2

---
+++ src/main/java/com/acmutv/ontoqa/examples/Example2A.java
@@ -0,0 +1,103 @@
+/**
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2016 Antonella Botte, Giacomo Marciani and Debora Partigianoni
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.acmutv.ontoqa.examples;
+
+import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.Model;
+import org.eclipse.rdf4j.model.Value;
+import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.model.impl.LinkedHashModel;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+import org.eclipse.rdf4j.model.vocabulary.RDF;
+import org.eclipse.rdf4j.model.vocabulary.RDFS;
+import org.eclipse.rdf4j.query.QueryResults;
+import org.eclipse.rdf4j.repository.Repository;
+import org.eclipse.rdf4j.repository.RepositoryConnection;
+import org.eclipse.rdf4j.repository.sail.SailRepository;
+import org.eclipse.rdf4j.repository.util.Repositories;
+import org.eclipse.rdf4j.rio.RDFFormat;
+import org.eclipse.rdf4j.rio.Rio;
+import org.eclipse.rdf4j.sail.Sail;
+import org.eclipse.rdf4j.sail.inferencer.fc.ForwardChainingRDFSInferencer;
+import org.eclipse.rdf4j.sail.memory.MemoryStore;
+
+import java.io.OutputStreamWriter;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * This class realizes the example 2.A
+ * @author Giacomo Marciani {@literal <gmarciani@acm.org>}
+ * @since 1.0
+ */
+public class Example2A {
+
+  public static void main(String[] args) {
+    String ns = "http://example.org/";
+
+    ValueFactory vf = SimpleValueFactory.getInstance();
+    IRI mortalIRI = vf.createIRI(ns, "Mortal");
+    IRI personIRI = vf.createIRI(ns, "Person");
+    IRI socratesIRI = vf.createIRI(ns, "Socrates");
+
+    // buona strategia in cui posso penalizzare le scritture mantenendo efficienti le lettura.
+    Sail sail = new ForwardChainingRDFSInferencer(new MemoryStore());
+    Repository repo = new SailRepository(sail);
+
+    try {
+      repo.initialize();
+
+      try (RepositoryConnection conn = repo.getConnection()) {
+        conn.add(mortalIRI, RDF.TYPE, RDFS.CLASS);
+        conn.add(mortalIRI, RDFS.LABEL, vf.createLiteral("Mortal","en"));
+        conn.add(mortalIRI, RDFS.LABEL, vf.createLiteral("Mortale","it"));
+
+        conn.add(personIRI, RDF.TYPE, RDFS.CLASS);
+        conn.add(personIRI, RDFS.LABEL, vf.createLiteral("Person","en"));
+        conn.add(personIRI, RDFS.LABEL, vf.createLiteral("Persona","it"));
+        conn.add(personIRI, RDFS.SUBCLASSOF, mortalIRI);
+
+        conn.add(socratesIRI, RDF.TYPE, personIRI);
+        conn.add(socratesIRI, RDFS.LABEL, vf.createLiteral("Socrates","en"));
+        conn.add(socratesIRI, RDFS.LABEL, vf.createLiteral("Socrate","it"));
+      }
+
+      Repositories.consume(repo, conn -> {
+        conn.export(Rio.createWriter(RDFFormat.TURTLE, System.out));
+      });
+
+      Set<Value> socratesTypes =  Repositories.get(repo, repoConn -> {
+        return QueryResults.asModel(repoConn.getStatements(socratesIRI, RDF.TYPE, null, true)).objects();
+      });
+
+      System.out.println("# RESULT #");
+      System.out.println(socratesTypes);
+
+    } finally {
+      repo.shutDown();
+    }
+  }
+}
---
+++ src/main/java/com/acmutv/ontoqa/examples/Example2B.java
@@ -0,0 +1,93 @@
+/**
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2016 Antonella Botte, Giacomo Marciani and Debora Partigianoni
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.acmutv.ontoqa.examples;
+
+import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.Value;
+import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+import org.eclipse.rdf4j.model.vocabulary.RDF;
+import org.eclipse.rdf4j.model.vocabulary.RDFS;
+import org.eclipse.rdf4j.query.BindingSet;
+import org.eclipse.rdf4j.query.QueryResults;
+import org.eclipse.rdf4j.query.TupleQuery;
+import org.eclipse.rdf4j.query.TupleQueryResult;
+import org.eclipse.rdf4j.repository.Repository;
+import org.eclipse.rdf4j.repository.RepositoryConnection;
+import org.eclipse.rdf4j.repository.sail.SailRepository;
+import org.eclipse.rdf4j.repository.util.Repositories;
+import org.eclipse.rdf4j.rio.RDFFormat;
+import org.eclipse.rdf4j.rio.Rio;
+import org.eclipse.rdf4j.sail.Sail;
+import org.eclipse.rdf4j.sail.inferencer.fc.ForwardChainingRDFSInferencer;
+import org.eclipse.rdf4j.sail.memory.MemoryStore;
+
+import java.io.IOException;
+import java.util.Set;
+
+/**
+ * This class realizes the example 2.b
+ * @author Giacomo Marciani {@literal <gmarciani@acm.org>}
+ * @since 1.0
+ */
+public class Example2B {
+
+  public static void main(String[] args) {
+    String ns = "http://example.org/";
+
+    ValueFactory vf = SimpleValueFactory.getInstance();
+    //IRI mortalIRI = vf.createIRI(ns, "Mortal");
+    //IRI personIRI = vf.createIRI(ns, "Person");
+    //IRI socratesIRI = vf.createIRI(ns, "Socrates");
+
+    Sail sail = new ForwardChainingRDFSInferencer(new MemoryStore());
+    Repository repo = new SailRepository(sail);
+
+    try {
+      repo.initialize();
+
+      Repositories.consume(repo, repoConn -> {
+        try {
+          repoConn.add(Example2B.class.getResource("/examples/example2B.ttl"), null, RDFFormat.TURTLE);
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+        TupleQuery query = repoConn.prepareTupleQuery("SELECT ?x WHERE {?x a <http://example.org/Mortal>}");
+        query.setIncludeInferred(false);
+
+        try (TupleQueryResult res = query.evaluate()) {
+          while (res.hasNext()) {
+            BindingSet solution = res.next();
+            System.out.println("# RESULT # " + solution.getValue("x"));
+          }
+        }
+      });
+
+    } finally {
+      repo.shutDown();
+    }
+  }
+}
