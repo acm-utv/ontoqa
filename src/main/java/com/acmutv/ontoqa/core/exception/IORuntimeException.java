82696bf2fce95081f46a1c2cb4deb0c564f4543f
Antonella <antonellabotte91@gmail.com>
2017-01-07 17:20:30 +0100

    Add (Second) Simple Lexicon v.0

---
+++ src/main/java/com/acmutv/ontoqa/core/exception/IORuntimeException.java
@@ -0,0 +1,14 @@
+package com.acmutv.ontoqa.core.exception;
+
+public class IORuntimeException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	public IORuntimeException(String s) {
+		super(s);
+	}
+	public IORuntimeException(String s, Throwable t) {
+		super(s, t);
+	}
+	public IORuntimeException(Throwable t) {
+		super(t);
+	}
+}
