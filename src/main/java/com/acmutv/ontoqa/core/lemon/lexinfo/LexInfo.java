b3cb5682f8db6728dc22d8c93c14c85e64e81bb7
Antonella <antonellabotte91@gmail.com>
2017-03-01 17:25:24 +0100

    Update Lexinfo

--- src/main/java/com/acmutv/ontoqa/core/lemon/lexinfo/LexInfo.java
+++ src/main/java/com/acmutv/ontoqa/core/lemon/lexinfo/LexInfo.java
@@ -49,8 +49,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URL;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
