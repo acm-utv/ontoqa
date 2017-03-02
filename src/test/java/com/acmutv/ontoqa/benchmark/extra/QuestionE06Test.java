b3c2502736fbf99eccb52e63c87b65540649c1bd
Giacomo Marciani <giacomo.marciani@gmail.com>
2017-03-02 15:42:05 +0100

    superlative questions

--- src/test/java/com/acmutv/ontoqa/benchmark/extra/QuestionE06Test.java
+++ src/test/java/com/acmutv/ontoqa/benchmark/extra/QuestionE06Test.java
@@ -28,11 +28,18 @@ package com.acmutv.ontoqa.benchmark.extra;
 
 import com.acmutv.ontoqa.benchmark.Common;
 import com.acmutv.ontoqa.core.CoreController;
+import com.acmutv.ontoqa.core.exception.LTAGException;
 import com.acmutv.ontoqa.core.exception.OntoqaFatalException;
 import com.acmutv.ontoqa.core.exception.QueryException;
 import com.acmutv.ontoqa.core.exception.QuestionException;
 import com.acmutv.ontoqa.core.knowledge.answer.Answer;
 import com.acmutv.ontoqa.core.knowledge.answer.SimpleAnswer;
+import com.acmutv.ontoqa.core.semantics.base.statement.OperatorType;
+import com.acmutv.ontoqa.core.semantics.dudes.DudesTemplates;
+import com.acmutv.ontoqa.core.semantics.sltag.SimpleSltag;
+import com.acmutv.ontoqa.core.semantics.sltag.Sltag;
+import com.acmutv.ontoqa.core.semantics.sltag.SltagBuilder;
+import com.acmutv.ontoqa.core.syntax.ltag.LtagTemplates;
 import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryFactory;
 import org.apache.logging.log4j.LogManager;
@@ -45,6 +52,9 @@ import org.junit.Test;
 import java.io.IOException;
 
 import static com.acmutv.ontoqa.benchmark.Common.*;
+import static com.acmutv.ontoqa.core.semantics.TestAllSemantics.HEIGHT_IRI;
+import static com.acmutv.ontoqa.core.semantics.TestAllSemantics.PERSON_IRI;
+import static com.acmutv.ontoqa.core.semantics.TestAllSemantics.RDF_TYPE_IRI;
 
 /**
  * JUnit tests for questions of class [CLASS EXTRA-06].
@@ -80,11 +90,58 @@ public class QuestionE06Test {
    * @throws OntoqaFatalException when question cannot be processed due to some fatal errors.
    */
   @Test
-  @Ignore
-  public void test_manual() throws OntoqaFatalException, QuestionException, QueryException {
-    final Answer actual = CoreController.process(QUESTION);
-    //TODO
-    Assert.assertEquals(ANSWER, actual);
+  public void test_manual() throws OntoqaFatalException, QuestionException, QueryException, LTAGException, IOException {
+    /* what */
+    Sltag what = new SimpleSltag(LtagTemplates.wh("what"), DudesTemplates.what());
+    LOGGER.info("what:\n{}", what.toPrettyString());
+
+    /* is (copula) */
+    Sltag is = new SimpleSltag(
+        LtagTemplates.copula("is", "1", "2"),
+        DudesTemplates.copula("1", "2")
+    );
+    LOGGER.info("is:\n{}", is.toPrettyString());
+
+    /* the most valuable */
+    Sltag theMostValuable = new SimpleSltag(
+        LtagTemplates.adjectiveSuperlative("most valuable", "the", "np"),
+        DudesTemplates.adjectiveSuperlative(OperatorType.MAX, HAS_COMPANY_VALUE_IRI, "np"));
+    LOGGER.info("the most valuable:\n{}", theMostValuable.toPrettyString());
+
+    /* company */
+    Sltag company = new SimpleSltag(
+        LtagTemplates.classNoun("company", false),
+        DudesTemplates.type(RDF_TYPE_IRI, COMPANY_IRI)
+    );
+    LOGGER.info("company:\n{}", company.toPrettyString());
+
+    /* the most valuable company */
+    LOGGER.info("the most valuable company: processing...");
+    Sltag theMostValuableCompany = new SltagBuilder(theMostValuable)
+        .substitution(company, "np")
+        .build();
+    LOGGER.info("the most valuable company:\n{}", theMostValuableCompany.toPrettyString());
+
+    /* what is */
+    LOGGER.info("what is: processing...");
+    Sltag whatIs = new SltagBuilder(is)
+        .substitution(what, "1")
+        .build();
+    LOGGER.info("what is:\n{}", whatIs.toPrettyString());
+
+    /* who is the most valuable company */
+    LOGGER.info("what is the most valuable company: processing...");
+    Sltag whatIsTheMostValuableCompany = new SltagBuilder(whatIs)
+        .substitution(theMostValuableCompany, "2")
+        .build();
+    LOGGER.info("what is the most valuable company:\n{}", whatIsTheMostValuableCompany.toPrettyString());
+
+    /* SPARQL */
+    LOGGER.info("SPARQL query: processing...");
+    Query query = whatIsTheMostValuableCompany.convertToSPARQL();
+    LOGGER.info("SPARQL query:\n{}", query);
+
+    Common.test_query(query, ANSWER);
   }
 
   /**
