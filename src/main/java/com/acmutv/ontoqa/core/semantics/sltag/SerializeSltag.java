403f9e3c9102e734e2083f205cc6abbe2596c02c
Debora <debbypar@yahoo.it>
2017-03-14 10:48:18 +0100

    bug fix

--- src/main/java/com/acmutv/ontoqa/core/semantics/sltag/SerializeSltag.java
+++ src/main/java/com/acmutv/ontoqa/core/semantics/sltag/SerializeSltag.java
@@ -16,6 +16,7 @@ import java.util.List;
 import org.apache.jena.base.Sys;
 
 import com.acmutv.ontoqa.core.lexicon.LEntry;
+import com.acmutv.ontoqa.core.lexicon.LOtherForm;
 import com.acmutv.ontoqa.core.lexicon.LSense;
 import com.acmutv.ontoqa.core.lexicon.LSynBehavior;
 import com.acmutv.ontoqa.core.lexicon.LexiconElement;
@@ -123,7 +124,7 @@ public class SerializeSltag {
 	 **/
 	public static ElementarySltag getSltagNameOf()
 	{
-		Ltag ltag = LtagTemplates.nameOf("DP1");
+		Ltag ltag = LtagTemplates.nameOf("DP");
 		Dudes dudes = new SimpleDudes();
 		ElementarySltag sltag = new SimpleElementarySltag("name of", ltag, dudes);
 		return sltag;
@@ -136,18 +137,18 @@ public class SerializeSltag {
 	public static List<ElementarySltag> getAllElementarySltag() throws IOException
 	{
 		List<LEntry> list = SerializeSltag.getAllLexiconElement();
-		LEntry lexEl = new LEntry();
+		LEntry lEntry = new LEntry();
 		List<ElementarySltag> listSltag = new ArrayList<ElementarySltag>();
 		int i;
 		for(i=0; i<list.size(); i++)
 		{
-			lexEl = list.get(i);
-			switch(TYPE.valueOf(lexEl.getPartOfSpeech())) {
+			lEntry = list.get(i);
+			switch(TYPE.valueOf(lEntry.getPartOfSpeech())) {
 				case properNoun:
 				{
-					Ltag ltag =  LtagTemplates.properNoun(lexEl.getWrittenRep());
-				    Dudes dudes = DudesTemplates.properNoun(lexEl.getSenses().iterator().next().getReference());
-				    ElementarySltag sltag = new SimpleElementarySltag(lexEl.getWrittenRep(), ltag, dudes);
+					Ltag ltag =  LtagTemplates.properNoun(lEntry.getWrittenRep());
+				    Dudes dudes = DudesTemplates.properNoun(lEntry.getSenses().iterator().next().getReference());
+				    ElementarySltag sltag = new SimpleElementarySltag(lEntry.getWrittenRep(), ltag, dudes);
 				    listSltag.add(sltag);
 				    break;
 				}
@@ -165,25 +166,82 @@ public class SerializeSltag {
 				}
 				case commonNoun:
 				{
-//					System.out.println("commonNoun: "+lexEl.getWrittenRep());
+					System.out.println("commonNoun: "+lEntry.getWrittenRep());
+					
+					
+					  System.out.println("WrittenRep Form: " + lEntry.getWrittenRep());
+					  System.out.println("CanonicalForm:   " + lEntry.getCanonicalForm());
+					  System.out.println("Part OF Speech:  " + lEntry.getPartOfSpeech());
+					  System.out.println("Decomposition:   " + lEntry.isDecomposition() +"\n");
 					  
-					Iterator ls = lexEl.getSenses().iterator();
-//					Ltag ltag = LtagTemplates.classNoun(list.get(i).getPartOfSpeech(), true);
-					while(ls.hasNext()){
-//					System.out.println("edges "+ltag.getEdges());
-						LSense lSense = (LSense) ls.next();
-						
-						/* LTAG */
-						Ltag ltag = LtagTemplates.classNoun(lexEl.getWrittenRep(), true);
-//						System.out.println("edges "+ltag.getEdges());
+					  Iterator otherFormsIt = lEntry.getOtherForms().iterator();
+					  int j=0;
+					  while(otherFormsIt.hasNext()){
+						  
+						  LOtherForm fo = (LOtherForm) otherFormsIt.next();
+						  System.out.println("Form n.      "+ j);
+						  System.out.println("Number       "+ fo.getNumber());
+						  System.out.println("Tense        "+ fo.getTense());
+						  System.out.println("WrittenRep   "+fo.getWrittenRep()+ "\n");
+						  j++;	  
+					  }
+					  
+					  /* Get all SynBehaviors*/
+					  Iterator synBehIt = lEntry.getSynBehaviors().iterator();
+					  int k=0;
+					  while(synBehIt.hasNext()){
+						  System.out.println("SynBeh n.  "+ k);
+						  LSynBehavior synB = (LSynBehavior) synBehIt.next();
+						  System.out.println("SynB URI   "+   synB.getFrame());
+						  System.out.println("AdverbialComplement   "+   synB.isFrameAdverbialComplement());
+						  System.out.println("FrameAttributiveArg   "+    synB.isFrameAttributiveArg());
+						  System.out.println(" CopulativeSubject    "+synB.isFrameCopulativeSubject());
+						  System.out.println("DirectObject          "+synB.isFrameDirectObject());
+						  System.out.println("PossessiveAdjunct     "+synB.isFramePossessiveAdjunct());
+						  System.out.println("PrepositionalObject   "+synB.isFramePrepositionalObject());
+						  System.out.println("Subject                "+synB.isFrameSubject()+ "\n");
+						  k++;
+						  
+					  }
+					  
+					  /* Get all Senses*/
+					  Iterator it =lEntry.getSenses().iterator();
+					  int w = 0;
+					  while(it.hasNext()){
+						 
+						  LSense ls = (LSense) it.next();
+						  System.out.println("Sensw num:      "+ w);
+						  System.out.println("Sense :         " + ls.getSense());
+						  System.out.println("Reference :     " +ls.getReference());
+						  System.out.println("ObjOfProp :     " +ls.getObjOfProp());
+						  System.out.println("SubjOfProp :    " +ls.getSubjOfProp());
+						  System.out.println("Is A :          "+ ls.getIsA() +"\n\n");
+						  w++;
+					  }
+					  
+					  
+//					  System.out.println("\n\n\n\n");
 
-						/* DUDES TODO */ 
-					    Dudes dudes = DudesTemplates.classNoun(lSense.getReference(), false);
+//					if(/*possessiveAdjunct = true*/)
 						
-					    ElementarySltag sltag = new SimpleElementarySltag(lexEl.getWrittenRep(), ltag, dudes);
-					    listSltag.add(sltag);
-					    
-					}
+					
+//					Iterator ls = lexEl.getSenses().iterator();
+////					Ltag ltag = LtagTemplates.classNoun(list.get(i).getPartOfSpeech(), true);
+//					while(ls.hasNext()){
+////					System.out.println("edges "+ltag.getEdges());
+//						LSense lSense = (LSense) ls.next();
+//						
+//						/* LTAG */
+//						Ltag ltag = LtagTemplates.classNoun(lexEl.getWrittenRep(), true);
+////						System.out.println("edges "+ltag.getEdges());
+//
+//						/* DUDES TODO */ 
+//					    Dudes dudes = DudesTemplates.classNoun(lSense.getReference(), false);
+//						
+//					    ElementarySltag sltag = new SimpleElementarySltag(lexEl.getWrittenRep(), ltag, dudes);
+//					    listSltag.add(sltag);
+//					    
+//					}
 
 
 					break;
@@ -198,30 +256,7 @@ public class SerializeSltag {
 //					for founded il template è il seguente
 //					Ltag ltag =  LtagTemplates.transitiveVerbActiveIndicative(list.get(i).getName(), "DP1", "DP2");
 
-					System.out.println("verb: "+lexEl.getWrittenRep());
-
-					System.out.println("senses: "+list.get(i).getSenses().get(0).getSense());
-					System.out.println("Reference :     " +list.get(i).getSenses().get(0).getReference());
-					  System.out.println("ObjOfProp :     " +list.get(i).getSenses().get(0).getObjOfProp());
-					  System.out.println("SubjOfProp :    " +list.get(i).getSenses().get(0).getSubjOfProp());
-					  System.out.println("Is A :          "+ list.get(i).getSenses().get(0).getIsA() +"\n\n");
-					  Iterator synBehIt = lexEl.getSynBehaviors().iterator();
-					  int k=0;
-						System.out.println("Syn Beh size: "+list.get(i).getSynBehaviors().size());
-					  while(synBehIt.hasNext()){
-						  System.out.println("SynBeh n.  "+ k);
-						  LSynBehavior synB = (LSynBehavior) synBehIt.next();
-						  System.out.println("SynB URI   "+   synB.getFrame());
-						  System.out.println("AdverbialComplement   "+   synB.isFrameAdverbialComplement());
-						  System.out.println("FrameAttributiveArg   "+    synB.isFrameAttributiveArg());
-						  System.out.println(" CopulativeSubject    "+synB.isFrameCopulativeSubject());
-						  System.out.println("DirectObject          "+synB.isFrameDirectObject());
-						  System.out.println("PossessiveAdjunct     "+synB.isFramePossessiveAdjunct());
-						  System.out.println("PrepositionalObject   "+synB.isFramePrepositionalObject());
-						  System.out.println("Subject                "+synB.isFrameSubject()+ "\n");
-						  k++;
-					  }
-
+					System.out.println("verb: "+lEntry.getWrittenRep());
 					
 //					for acquire TODO ....
 					break;
@@ -261,6 +296,7 @@ public class SerializeSltag {
 //			System.out.println("semantics: "+listSltag.get(i).getSemantics());
 			System.out.println("edges: "+listSltag.get(i).getEdges());
 		}
+
 		
 //		System.out.println(LtagTemplates.classNoun("prova", true).getEdges());
 		return listSltag;
